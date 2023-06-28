c526a2c5e3ddaa696f948953d0a5b26476f8510e
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-06-29 00:44:17 +0530

    Create index.js
    create file index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Package: logstrail
+ * Author: Ganesh B
+ * Description: 
+ * Install: npm i logstrail --save
+ * Github: https://github.com/ganeshkbhat/logstrail
+ * npmjs Link: https://www.npmjs.com/package/logstrail
+ * File: .js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
